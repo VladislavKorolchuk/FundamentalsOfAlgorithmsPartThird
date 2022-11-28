9cfd1fb6354af6859d0d0e1cf56eaf045dc89b02
93294 <9329496@mail.ru>
2022-11-28 16:37:04 +0300

    Initial commit

---
+++ src/main/java/com/example/fundamentalsofalgorithmspartthird/FundamentalsOfAlgorithmsPartThirdApplication.java
@@ -0,0 +1,13 @@
+package com.example.fundamentalsofalgorithmspartthird;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentalsOfAlgorithmsPartThirdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundamentalsOfAlgorithmsPartThirdApplication.class, args);
+    }
+
+}
